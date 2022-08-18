033578e5943efde18f0f84b20780bb1c89e2e864
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-17 19:41:42 -0600

    feat: add message to exception filter

--- src/common/exceptions/filters/http-exception.filter.ts
+++ src/common/exceptions/filters/http-exception.filter.ts
@@ -13,11 +13,13 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+    const message = exception.getResponse();
 
     response.status(status).json({
       statusCode: status,
       timestamp: new Date().toISOString(),
       path: request.url,
+      message,
     });
   }
 }
